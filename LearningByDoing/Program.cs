1b9d34e0e15e11de053fa3191056ea85b84712eb
Novendra <novendra@ptcode.id>
2025-06-03 11:09:08 +0700

    init

---
+++ LearningByDoing/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapDefaultControllerRoute();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
